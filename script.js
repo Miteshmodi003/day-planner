97ed975a5eb770aa8265e92dd5b6e9384558a524
Miteshmodi003 <mitesh.modi003@gmail.com>
2020-04-04 22:54:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    let timeNow = moment().hour(), 
+        dailyHours = [22, 23, 24, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        // [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21], 
+        textAreaIdSlots = ["notes-info-9am", "notes-info-10am", "notes-info-11am", "notes-info-12pm", "notes-info-1pm", "notes-info-2pm", "notes-info-3pm",
+                        "notes-info-4pm", "notes-info-5pm", "notes-info-6pm", "notes-info-7pm", "notes-info-8pm", "notes-info-9pm"];
+
+    let currentDate = moment().format('dddd, MMMM Do YYYY');        
+    $("#currentDate").text(currentDate);
+    var saveBtn = $(".saveBtn");
+    saveBtn.click(function () {
+        // $(this).attr("class", "btn btn-success saveBtn");
+        let saveBtnDataId = $(this).attr("data-id");
+        let userNote = $("#" + "notes-info-" + saveBtnDataId).val();        
+        localStorage.setItem("notes-info-"+saveBtnDataId, userNote);
+    });
+
+    function catergorizeTimeSlotsByColorCode() {
+        let currentTime = timeNow, 
+            calenderHours = dailyHours, 
+            hourSlots = ["notes-info-9am", "notes-info-10am", "notes-info-11am", "notes-info-12pm", "notes-info-1pm", "notes-info-2pm", "notes-info-3pm",
+                        "notes-info-4pm", "notes-info-5pm", "notes-info-6pm", "notes-info-7pm", "notes-info-8pm", "notes-info-9pm"]; 
+            
+        for (var i = 0; i < calenderHours.length; i++) {
+            let currentHourSlot = $("#" + hourSlots[i]);
+            // retrieving info from local storage for each time block
+            currentHourSlot.val(localStorage.getItem(hourSlots[i] + "-info"));
+            
+            if (calenderHours[i] < currentTime){
+                saveBtn.attr("class", "btn btn-secondary disabled saveBtn")
+                currentHourSlot.css("background-color", "light grey");
+                currentHourSlot.prop("disabled", true);
+            } 
+            else if (calenderHours[i] === currentTime) {
+                saveBtn.attr("class", "btn btn-danger saveBtn")
+                currentHourSlot.css("color", "white");
+                currentHourSlot.css("background-color", "#a30311");
+            }
+            else if (calenderHours[i] > currentTime) {
+
+                currentHourSlot.css("color", "#525451");
+                currentHourSlot.css("background-color", "#bff590");
+            };
+            
+        };
+    };
+
+    catergorizeTimeSlotsByColorCode();
+});
